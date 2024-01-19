fa1cf839944d95ff81bbf3ed74805da556d1aa72
CMeraz13 <meraz.cesar13@gmail.com>
2024-01-18 18:16:43 -0700

    Learning Activity W02
    Created and completed Learning Activity W02

---
+++ Week 1/W02-Learning-Activity.js
@@ -0,0 +1,78 @@
+
+
+// Adding two numbers together
+
+x = 7;
+y = 13;
+
+z = x+y;
+console.log(z);
+
+// Multipy two numbers together
+
+z = x*y;
+
+console.log(z);
+
+// Subtracting two numbers from each other
+
+z = y-x;
+console.log(z);
+
+// Using Strings to create a sentence
+
+w1 = "Welcome ";
+w2 = "User!";
+
+w1 += w2;
+console.log(w1);
+
+// Assigning variable
+
+var e = 6;
+
+// Incrementing a variable by three
+var q = e;
+q += 3;
+
+console.log(q);
+
+// Comparing Values
+
+if(x == e){
+    comparison_equal_true = "They are the same!";
+    console.log(comparison_equal_true);
+} else {
+    comparison_equal_false = "They are not the same!";
+    console.log(comparison_equal_false);
+}
+
+// Checking if a number is less than another number
+
+if(q < y){
+    comparison_less_than_true = "Variable "+ q + " is less than Variable " + y;
+    console.log(comparison_less_than_true);
+} else {
+    comparison_less_than_false = "Variable " + y + " is greater than Variable "+ q;
+    console.log(comparison_less_than_false);
+}
+
+// Checking if two numbers are NOT equal
+
+if(z != e){
+    comparison_not_equal_true = "Variable " + z + " is not equal to Variable " + e;
+    console.log(comparison_not_equal_true);
+} else {
+    comparison_not_equal_false = "Variable " + z + " is equal to Variable " + e;
+    console.log(comparison_not_equal_false)
+}
+
+// Checking to see if a value is LESS than ten AND GREATER than 0
+
+if(e < 10 && e > 0){
+    comparison_less_than_and_greater_than_true = "Variable " + e + " is less than 10 and greater than 0!";
+    console.log(comparison_less_than_and_greater_than_true);
+} else {
+    comparison_less_than_and_greater_than_false = "Variable " + e + " is not less than 10 and greater than 0!";
+    console.log(comparison_less_than_and_greater_than_false);
+}
