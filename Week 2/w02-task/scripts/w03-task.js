0bd6d636178db77b19f11141c2cc67e753bff7a1
CMeraz13 <meraz.cesar13@gmail.com>
2024-01-27 17:49:15 -0700

    Finished w03 Programming task

--- Week 2/w02-task/scripts/w03-task.js
+++ Week 2/w02-task/scripts/w03-task.js
@@ -13,7 +13,8 @@ function addNumbers(){
 
 document.querySelector("#addNumbers").addEventListener("click", addNumbers);
 
-/* Function Expression - Subtract Numbers 
+/*Function Expression - Subtract Numbers */
+
 function subtract(number1, number2){
     return number1 - number2;
 }
@@ -27,22 +28,61 @@ document.querySelector("#subtractNumbers").addEventListener("click", subtractNum
 
 /* Arrow Function - Multiply Numbers */
 
+const multiply = (number1, number2) => number1 * number2;
+
+function multiplyNumbers() {
+    var factor1 = document.querySelector("#factor1").value;
+    var factor2 = document.querySelector("#factor2").value;
+    document.querySelector("#product").value = multiply(factor1, factor2);
+}
+document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
+
+
 
 /* Open Function Use - Divide Numbers */
 
+function divide(number1, number2){
+    return number1 / number2;
+}
+function divideNumbers(){
+    var dividend = document.querySelector("#dividend").value;
+    var divisor = document.querySelector("#divisor").value;
+    document.querySelector("#quotient").value = divide(dividend, divisor);
+}
+document.querySelector("#divideNumbers").addEventListener("click", divideNumbers);
 
 /* Decision Structure */
 
+document.querySelector("#getTotal").addEventListener("click", function(){
+    let subTotal = parseFloat(document.querySelector("#subtotal").value);
+    console.log("Subtotal", subTotal);
+    let applyDiscount = document.querySelector("#member").checked;
+    if (applyDiscount){
+        subTotal *= 0.8; // Applying 20% discount
+    }
+
+    let totalDue = `$${subTotal.toFixed(2)}`; // Template literal for string interpolation
+    document.querySelector("#total").textContent = totalDue;
+    console.log(totalDue);
+});
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 
+let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+document.querySelector("#array").textContent = `[${arrayNumbers.join(', ')}]`
+
 /* Output Odds Only Array */
+document.querySelector("#odds").innerHTML = arrayNumbers.filter(number => number % 2 !== 0);
 
 /* Output Evens Only Array */
+document.querySelector("#evens").innerHTML = arrayNumbers.filter(number => number % 2 === 0);
 
 /* Output Sum of Org. Array */
+document.querySelector("#sumOfArray").innerHTML = arrayNumbers.reduce((sum, number) => sum + number);
 
 /* Output Multiplied by 2 Array */
+document.querySelector("#multiplied").innerHTML = arrayNumbers.map(number => number * 2);
 
 /* Output Sum of Multiplied by 2 Array */
+document.querySelector("#sumOfMultiplied").innerHTML = arrayNumbers.map(number => number * 2).reduce((sum, number) => sum + number, 0);
